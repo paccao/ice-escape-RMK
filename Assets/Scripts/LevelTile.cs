cdb71665d2ff6a8cff48c52635bde0ca5e36779b
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-05-21 08:47:38 +0200

    created first snow level

--- Assets/Scripts/LevelTile.cs
+++ Assets/Scripts/LevelTile.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(fileName = "New Level Tile", menuName = "2D/Tiles/Level Tile")]
 public class LevelTile : Tile
